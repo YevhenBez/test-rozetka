9977cec28cb3f69f8f8553fc640fa5c3a0c29717
YevhenBez <byxgalter@bigmir.net>
2023-11-16 12:57:25 +0200

    Создал компонент   Title   и прописал его в файле   App.jsx   Тестовое для Rozetka
    Создал компонент   Title  в файле   Title.jsx   Затем импортнул его в файле   App.jsx   и разрендерил. Также в файле   index.js   сделал реэкспорт компонента   Title  Тестовое для Rozetka

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import Title from './title';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +12,9 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <div>
+        <Title />
+      </div>
     </div>
   );
 };
---
+++ src/components/title/Title.jsx
@@ -0,0 +1,8 @@
+const Title = () => {
+
+  return (
+    <div>Здесь будет титул данного сайта</div>
+  );
+};
+
+export default Title;
